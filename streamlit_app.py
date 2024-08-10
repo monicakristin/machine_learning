b273e7b58ec6d3b992859d308a292f01d7a74bcb
Monica Kristin N <139729665+monicakristin@users.noreply.github.com>
2024-08-10 22:48:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,5 @@ with st.expander('Data'):
   st.write('**y**')
   y = df.species
   y
+with st.expander('Data Visualization'):
+  st.scatter_chart(data=df, x = 'bill_length_mm', y = 'body_mass_g', color='species')
