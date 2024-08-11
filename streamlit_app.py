7f091513a5c271fddce6cd5f90ff5b8255b21bf6
Monica Kristin N <139729665+monicakristin@users.noreply.github.com>
2024-08-11 12:28:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,5 +67,9 @@ prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 prediction_proba
 
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.column= ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba
+
 
 
