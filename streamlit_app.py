763ec8f83bce5285df0f7e66652b93ef39c20260
Monica Kristin N <139729665+monicakristin@users.noreply.github.com>
2024-08-11 11:02:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.sidebar:
        'bill_depth_mm':  bill_depth_mm,
        'flipper_length_mm': flipper_length_mm,
        'body_mass_g': body_mass_g,
-       'sex': gender}
+       'gender': gender}
   input_df = pd.DataFrame(data, index = [0])
   input_penguins = pd.concat([input_df, X], axis = 0)
 with st.expander('Input Features'):
